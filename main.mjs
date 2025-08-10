325ec33aa5af5b76c7ec379ef6ab598c2788249c
Christian R <github@chrisnew.de>
2025-08-10 15:19:28 +0300

    first meaningful™ commit

--- main.mjs
+++ main.mjs
@@ -3,7 +3,7 @@ import { ServerGameAPI } from './GameAPI.mjs';
 import { ClientGameAPI } from './client/ClientAPI.mjs';
 
 const identification = {
-  name: 'Quake',
+  name: 'Hellwave',
   author: 'chrisnew',
   version: [1, 0, 0],
   capabilities: [
